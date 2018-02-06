95e94b9b17964393cd195f497562c6f33aaafee9
Sicong Gao <siconggao@yahoo.com>
2018-02-06 11:28:57 +1100

    add modify auther

--- src/main.f
+++ src/main.f
@@ -1,9 +1,9 @@
 ! ************************************************
-! Forest Light Environmental Simulator (FLiES)    
+! Forest Light Environmental Simulator (FLiES)
 !
 !     Version 2.4.2
 !     Hideki Kobayashi
-! 
+!
 !    as of 2013.11.19
 !
 !     H. Kobayashi and H. Iwabuchi (2008),
@@ -15,7 +15,7 @@
 c      include 'mpif.h'
       include 'common.inc'
       include 'math.inc'
-    
+
 !     --- only in main
       integer i,up,ip,iwl,iz, num
       integer knmix,knang,knzext
@@ -44,17 +44,17 @@ c      include 'mpif.h'
       real*8 tpfd,bpfd,dpfd,obpfd,odpfd
       real*8 rflx, rbflx, rdflx
       real*8 scmpf(3,100),scmpp(3,100)
-      
+
       real RF,RQ
       real fpc,fpf,fpv(100)
 
-      real ext(knmix, knzext)  
+      real ext(knmix, knzext)
 
 !     --- set by rparam
       integer np
       integer npl(200),nmix
 !     --- fmode for SIF
-      integer amode,imode,cmode,fmode 
+      integer amode,imode,cmode,fmode
       integer stype
       real alb(100)
       real dif,phi,th,ph, tgx, tgy
@@ -69,7 +69,7 @@ c      include 'mpif.h'
 !     MPI related parapeters
       integer Nid, Nprod, ierr
       integer npproc
-      
+
       Nid = 0
       Nprod = 1
 
@@ -81,11 +81,13 @@ c      call MPI_comm_rank(MPI_COMM_WORLD,Nid,ierr)
 
       if(Nid .eq. 0) then
       write(*,*) "*********************************************"
-      write(*,*) "3D canopy-atmosphere radiative transfer model" 
+      write(*,*) "3D canopy-atmosphere radiative transfer model"
       write(*,*) "Forest Light Environmental Simulator (FLiES) "
       write(*,*) "                         by Hideki Kobayashi "
       write(*,*) "        Special thanks to Hironobu Iwabuchi  "
       write(*,*) "               Version 2.4.* Since 2008/5/1  "
+      write(*,*) "               Add SIF module by Sicong Gao  "
+      write(*,*) "                                   2018.2.5  "
       write(*,*) "*********************************************"
       end if
 
@@ -114,7 +116,7 @@ c      call MPI_comm_rank(MPI_COMM_WORLD,Nid,ierr)
       num = 5
 c      num = 51
 c      open(51, file = "init.inp")
-      
+
 !     Read required parameters
       write(*,*) "rparam"
       call rparam(
@@ -123,7 +125,7 @@ c      open(51, file = "init.inp")
      &     dif,th0,ph0,phi,th,ph,
      &     sinf0,cosf0,cosq0,sinq0,
      &     wq,span,wl0,wls,RF,RQ,
-     &     d,spcf,spcq,taur, 
+     &     d,spcf,spcq,taur,
      &     ctaur,rfname,fname,alb,
      &     lr,lt,ulr,ult,str,sor,
      &     npproc,Nprod, num)
@@ -146,11 +148,11 @@ c      open(51, file = "init.inp")
          w = 1.
 
          write(*,*) "start fish eye"
-         call vegfeye(w, tgx, tgy, ichi, ikd)              
+         call vegfeye(w, tgx, tgy, ichi, ikd)
 
          write(*,*) "wdata"
-         call wdata(np, nwl, stype, imode, cmode, amode, span, 
-     &     RF, RQ, cosq0, wl0, wls, tflx, bflx, dflx, 
+         call wdata(np, nwl, stype, imode, cmode, amode, span,
+     &     RF, RQ, cosq0, wl0, wls, tflx, bflx, dflx,
      &     tpfd, bpfd, dpfd, scmpf, scmpp, rflx, rbflx, rdflx)
          stop
       end if
@@ -160,7 +162,7 @@ c      open(51, file = "init.inp")
          write(*,*) "input sampling grid (m)"
          write(*,*) "suggested value is 0.1 (m)"
          read(num,*) spn
-         call clai(lai, plai, spn, cv) 
+         call clai(lai, plai, spn, cv)
          write(*,*) "LAI = ", lai, "; Crown cover (%) = ", cv * 100.
          write(*,*) "H(m) "," LAI_profile"
          do i = int(zmax + 1), 1, -1
@@ -171,7 +173,7 @@ c      open(51, file = "init.inp")
 
 c      close(51)
 
-!     Interval to show the progress in standard I/O 
+!     Interval to show the progress in standard I/O
       if(cmode.eq.1)then
          up = 1000
       else
@@ -187,8 +189,8 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
 
       write(*,*) "start simulation"
 
-      if(amode .eq. 2)then      !     without atmosphere 
-         
+      if(amode .eq. 2)then      !     without atmosphere
+
          do ip = 1, npproc
 
             if(mod(ip, up) .eq. 0)  write(*,*) ip," of ", npproc
@@ -200,7 +202,7 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
 
                ux = sinq0 * cosf0
                uy = sinq0 * sinf0
-               uz = cosq0                  
+               uz = cosq0
                if(abs(uz) .lt. 0.0174524) uz = sign(0.0174524, uz)
                nscat = 0
                nscata = nscat
@@ -209,7 +211,7 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
 
                th = 0.5 * pi + 0.5 * acos(1. - 2. * real(frnd()))
                ph = 2.0 * pi * real(frnd())
-                   
+
                ux = fsin(th) * fcos(ph)
                uy = fsin(th) * fsin(ph)
                uz = fcos(th)
@@ -226,7 +228,7 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
             tpfd = tpfd + w * wq
 
             bflx = bflx + w * (1. - min(real(nscat), 1.))
-            bpfd = bpfd + w * wq * (1. - min(real(nscat), 1.))                 
+            bpfd = bpfd + w * wq * (1. - min(real(nscat), 1.))
             dflx = dflx + w * min(real(nscat), 1. )
             dpfd = dpfd + w * wq * min(real(nscat), 1.)
 
@@ -250,40 +252,40 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
                   tstr(i) = str(i,1)
                end do
 
-!     call the canopy radiation transfer module 
+!     call the canopy radiation transfer module
 
                call canort(x, y, ux, uy, uz, w, wq, cmode, nscat,
      &        tlr, tlt, ulr(1), ult(1), tstr, sor(1), ichi, ikd)
 
-               rflx = rflx + w               
-               rbflx = rbflx + w * (1. - min(real(nscata), 1.))                 
+               rflx = rflx + w
+               rbflx = rbflx + w * (1. - min(real(nscata), 1.))
                rdflx = rdflx + w * min(real(nscata), 1. )
             end if
 
          end do
-         
+
       else                      !     With atmosphere
-         
+
          write(*,*) "# of sampling wavelength"
          do iwl = 1, nwl
             write(*, *) iwl, "  of", nwl, " (", npl(iwl),") "
-            
-!     preparation of the atmospheric optical parameters         
-            call  prepatm(imode, wq, RQ, span, spcq, np, npproc, 
-     &           wl0, wls, iwl, npl, nkd, wkd, ext, rfname, fname, 
-     &           nmix, cflg, re, G, Qabs, Qext, Qext_ref, d, 
+
+!     preparation of the atmospheric optical parameters
+            call  prepatm(imode, wq, RQ, span, spcq, np, npproc,
+     &           wl0, wls, iwl, npl, nkd, wkd, ext, rfname, fname,
+     &           nmix, cflg, re, G, Qabs, Qext, Qext_ref, d,
      &           cbnz, ctnz, taur, ctaur)
 
             if(npl(iwl) .eq. 0) then
                write(*, *) "npl error !"
-               exit 
+               exit
             end if
 
 !     loop for single wavelength
             do ip = 1, npl(iwl)
 
                if(mod(ip, up) .eq. 0)  write(*,*) ip," of ", npl(iwl)
- 
+
                w = 1.0
                x = xmax * real(frnd())
                y = ymax * real(frnd())
@@ -291,7 +293,7 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
                ux = sinq0 * cosf0
                uy = sinq0 * sinf0
                uz = cosq0
-               
+
                ftau = -log(max(1.0e-35, real(frnd())))
                chi = 1.0
                ichi = 1
@@ -318,11 +320,11 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
      &                 + w * min(real(nscat), 1. )
 
                   scmpp(1, iwl) = scmpp(1, iwl) + w * wq
-                  scmpp(2, iwl) = scmpp(2, iwl) 
-     &                 + w * wq * (1. - min(real(nscat), 1.))                 
-                  scmpp(3, iwl) = scmpp(3, iwl) 
+                  scmpp(2, iwl) = scmpp(2, iwl)
+     &                 + w * wq * (1. - min(real(nscat), 1.))
+                  scmpp(3, iwl) = scmpp(3, iwl)
      &                 + w * wq * min(real(nscat), 1.)
-                  
+
 !     surface reflection
                   if(stype .eq. 1) then !     lambertian
                      w = w * alb(1)
@@ -331,31 +333,31 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
 
                      th = 0.5 * facos(1.-2.* real(frnd()))
                      ph = 2 * pi * real(frnd())
-                     
+
                      ux = fsin(th) * fcos(ph)
                      uy = fsin(th) * fsin(ph)
                      uz = fcos(th)
                      if(abs(uz) .lt. 0.0174524) uz = sign(0.0174524, uz)
-                    
-                     rflx = rflx + w               
-                     rbflx = rbflx + w * (1. - min(real(nscata), 1.))      
+
+                     rflx = rflx + w
+                     rbflx = rbflx + w * (1. - min(real(nscata), 1.))
                      rdflx = rdflx + w * min(real(nscata), 1. )
-                   
-                  else          !     3-D surface                     
+
+                  else          !     3-D surface
                      do i = 1, nts
                         tlr(i) = lr(i,iwl)
                         tlt(i) = lt(i,iwl)
                         tstr(i) = str(i,iwl)
                      end do
-      
-!     call the canopy radiation transfer module 
+
+!     call the canopy radiation transfer module
                      call canort(x, y, ux, uy, uz, w, wq, cmode, nscat,
-     &                    tlr, tlt, ulr(iwl), ult(iwl), tstr, 
+     &                    tlr, tlt, ulr(iwl), ult(iwl), tstr,
      &                    sor(iwl), ichi, ikd)
                      if(w .lt. conv) exit
                      iz = 1
-                     rflx = rflx + w               
-                     rbflx = rbflx + w * (1. - min(real(nscata), 1.))      
+                     rflx = rflx + w
+                     rbflx = rbflx + w * (1. - min(real(nscata), 1.))
                      rdflx = rdflx + w * min(real(nscata), 1. )
                   end if
                end do           ! single photon loop
@@ -367,7 +369,7 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
             dflx = dflx + scmpf(3,iwl)
 
             tpfd = tpfd + scmpp(1,iwl)
-            bpfd = bpfd + scmpp(2,iwl)                 
+            bpfd = bpfd + scmpp(2,iwl)
             dpfd = dpfd + scmpp(3,iwl)
 
          end do                 ! loop for nwl (spectral loop)
@@ -379,16 +381,16 @@ c      call MPI_Barrier(MPI_COMM_WORLD,ierr)
 !     ****************************************
 
 !     ****************************************
-c      call mpisum(tflx, bflx, dflx, 
+c      call mpisum(tflx, bflx, dflx,
 c     &     tpfd, bpfd, dpfd, scmpf, scmpp, rflx, rbflx, rdflx)
 !     ***************************************
-      
+
 
 !     Write the simulated dataset
       if(Nid .eq. 0) then
          write(*,*) "wdata"
-         call wdata(np, nwl, stype, imode, cmode, amode, span, 
-     &        RF, RQ, cosq0, wl0, wls, tflx, bflx, dflx, 
+         call wdata(np, nwl, stype, imode, cmode, amode, span,
+     &        RF, RQ, cosq0, wl0, wls, tflx, bflx, dflx,
      &        tpfd, bpfd, dpfd, scmpf, scmpp, rflx, rbflx, rdflx)
       end if
 
